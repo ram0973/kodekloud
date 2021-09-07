c80b83ff1ee8c86e495ba5b436637d64392f45d1
ram0973 <ram0973@gmail.com>
2021-09-07 11:06:01 +0300

    init

---
+++ install_key.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Make keypair and copy it to servers
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+# set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+ssh-keygen
+chmod -R o-rwx,g-rwx ~/.ssh/*
