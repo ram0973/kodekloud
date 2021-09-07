9062aa124eba73c98ad011166b3c2b07135de443
ram0973 <ram0973@gmail.com>
2021-09-07 12:52:26 +0300

    fix install key

--- install_key.sh
+++ install_key.sh
@@ -13,49 +13,8 @@ set -o pipefail
 # set -o xtrace
 
 ssh-keygen
-chmod -R o-rwx,g-rwx ~/.ssh/*
 
-tee -a ~/.ssh/config << END
-Host stapp01
-  Hostname 172.16.238.10
-  User tony
-
-Host stapp02
-  Hostname 172.16.238.11
-  User steve
-
-Host stapp03
-  Hostname 172.16.238.12
-  User banner
-
-Host stdb01
-  Hostname 172.16.239.10
-  User peter
-END
-
-chmod -R o-rwx,g-rwx ~/.ssh/*
-ssh-copy-id -i ~/.ssh/id_rsa.pub tony@stapp01
-ssh-copy-id -i ~/.ssh/id_rsa.pub steve@stapp02
-ssh-copy-id -i ~/.ssh/id_rsa.pub banner@stapp03
-#ssh-copy-id -i ~/.ssh/id_rsa.pub peter@stdb01
-#!/usr/bin/env bash
-# Make keypair and copy it to servers
-
-# Exit on error. Append "|| true" if you expect an error.
-set -o errexit
-# Exit on error inside any functions or subshells.
-set -o errtrace
-# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
-# set -o nounset
-# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
-set -o pipefail
-# Turn on traces, useful while debugging but commented out by default
-# set -o xtrace
-
-ssh-keygen
-chmod -R o-rwx,g-rwx ~/.ssh/*
-
-tee -a ~/.ssh/config << END
+tee -a ~/.ssh/config < END
 Host stapp01
   Hostname 172.16.238.10
   User tony
@@ -96,5 +55,3 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub banner@stapp03
 #ssh-copy-id -i ~/.ssh/id_rsa.pub clint@stbkp01
 #ssh-copy-id -i ~/.ssh/id_rsa.pub groot@stmail01
 #ssh-copy-id -i ~/.ssh/id_rsa.pub jenkins@jenkins
-
-
